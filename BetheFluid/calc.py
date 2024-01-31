3979f252039f1d1bd030e951397058b5625b9964
antonilis <126914120+antonilis@users.noreply.github.com>
2024-01-31 20:05:07 +0100

    Abstracting code by introducing abstract classes in calc module and specifying models in calcLiebLiniger modul.

--- BetheFluid/calc.py
+++ BetheFluid/calc.py
@@ -1,21 +1,12 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
 
-class CalcV:
-    '''
-    Class calculating effective velocity of given state rho
-    '''
+# Module containing abstract classes for calculating effective velocity and diffusion operator
+
+class CalcV(ABC):
 
     def __init__(self, rho, l, c):
-        '''
-        Constructor of a class
-        Parameters
-        ----------
-        rho : numpy array, state
-        l : numpy array, momenta
-        c : tuple of floats, coupling constants
-        '''
-        # dimensions of rho x, l
         self.rho = self.get_rho(rho)
         self.l = l
         self.c = c
@@ -41,17 +32,9 @@ class CalcV:
 
         return rho
 
+    @abstractmethod
     def create_T(self):
-        '''
-        Returns
-        -------
-        numpy array, integral kernel of GHD
-        '''
-        l, u = np.meshgrid(self.l, self.l, indexing='ij')
-
-        T = self.c / np.pi * 1 / ((l - u) ** 2 + self.c ** 2)
-
-        return T
+        pass
 
     def get_n_rho_tot(self):
         '''
@@ -78,7 +61,7 @@ class CalcV:
         '''
 
         # dimensions : T(l,u) , n(x, u) -> Tn (x,l,u)
-        #Tn = self.T[np.newaxis, :, :] * self.n[:, np.newaxis, :]
+        # Tn = self.T[np.newaxis, :, :] * self.n[:, np.newaxis, :]
 
         Tn = np.einsum('lu, xu... -> xlu...', self.T, self.n, optimize=True)
 
@@ -122,9 +105,6 @@ class CalcV:
 
 
 class CalcD(CalcV):
-    '''
-    Class calculating diffusion operator for given state rho, derived class of CalcV
-    '''
 
     def __init__(self, rho, l, c):
         '''
@@ -137,66 +117,24 @@ class CalcD(CalcV):
         '''
         super().__init__(rho, l, c)
 
-        # dimensions N, x, l    
+        # dimensions N, x, l
         self.W = self.get_W()
         self.w = np.sum(self.W, axis=-2) * self.int_l
         self.D_ker = self.get_D_ker()
         self.D = self.get_D()
 
+    @abstractmethod
     def get_W(self):
-        '''
-        Calculates W operatos
-        Returns
-        -------
-        W : numpy array
-        '''
-        T_dr = np.einsum('xlu, uo -> xlo', self.operator, self.T, optimize=True)
+        pass
 
-        # Now order of indices is x, l, u
-
-        rho = self.rho[Ellipsis, np.newaxis]
-
-        n = self.n[Ellipsis, np.newaxis]
-
-        W = rho * (1 - n) * T_dr ** 2 * np.abs(self.V[Ellipsis, np.newaxis] - self.V[Ellipsis, np.newaxis, :])
-
-        return W
-
+    @abstractmethod
     def get_D_ker(self):
-        '''
-        Calculates D_ker operator, where D operator is  (1 -Tn)-1 rho D_ker rho-1 (1 -Tn)
-        Returns
-        -------
-        D_ker : numpy array
-        '''
-        delta = np.identity(self.l.size)[np.newaxis, Ellipsis]
+        pass
 
-        rho_tot = self.rho_tot[Ellipsis, np.newaxis]
+    @abstractmethod
+    def get_D_ker(self):
+        pass
 
-        # dimensions x, l, u
-        D_ker = (delta * self.w[Ellipsis, np.newaxis] - self.W * self.int_l) / rho_tot ** 2
-
-        return D_ker
-
+    @abstractmethod
     def get_D(self):
-        '''
-        Calculates diffusion operator
-        Returns
-        -------
-        D : numpy array
-        '''
-        Tn = self.T[np.newaxis, :, :] * self.n[:, np.newaxis, :]
-
-        delta = np.identity(self.l.size)[np.newaxis, Ellipsis]
-
-        op_ker = delta - Tn * self.int_l
-
-        rho_factor = self.rho_tot[Ellipsis, np.newaxis] / self.rho_tot[Ellipsis, np.newaxis, :]
-
-        D_ker = self.D_ker * rho_factor
-
-        D = np.einsum('xou, xul , xls -> xos', self.operator, D_ker, op_ker, optimize=True)
-
-        return D
-
-# Changes:  rho_factor is in different place and D_ker is devided by rho_tot**2
+        pass
